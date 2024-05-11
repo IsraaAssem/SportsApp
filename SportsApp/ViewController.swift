b61229f6f2695d23d3ffba819d52be9fa4d08e24
IsraaAssem <israaassem20@gmail.com>
2024-05-11 13:40:14 -0400

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Israa Assem on 11/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
